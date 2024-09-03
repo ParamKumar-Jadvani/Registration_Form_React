8e025329ee076e60c72085a1c06354294382f7bd
Param-Kumar Jadvani <jadvaniparam@gmail.com>
2024-09-03 23:04:28 +0530

    Display Product Compelete

--- src/DisplayProduct.jsx
+++ src/DisplayProduct.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./DisplayProduct.css";
 
 const DisplayProduct = ({ id, product }) => {
+  console.log(id);
+
   return (
     <div key={id} className="product-div">
       <img src={product.image} alt={product.title} />
       <h2>{product.title}</h2>
       <p>Price: {product.price}</p>
       <p>Category: {product.category}</p>
-      <button onClick={() => console.log("Delete product")}>Delete</button>
+      <button onClick={() => console.log("Delete product")}>
+        Delete
+      </button>
     </div>
   );
 };
